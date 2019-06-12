17bc051a6da50e9317153d596d106608016c87f0
Chris Underwood <cunderwoodmn@gmail.com>
2019-06-11 18:12:17 -0700

    made the list immutabel when deleting persons

--- src/App.js
+++ src/App.js
@@ -35,8 +35,14 @@ class App extends Component {
     });
   };
 
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons.slice();
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
+    const doesShow = this.state.showPersons; // or [...this.state.persons]
     this.setState({ showPersons: !doesShow });
   };
 
@@ -55,8 +61,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={index}
+              />
+            );
           })}
         </div>
       );
