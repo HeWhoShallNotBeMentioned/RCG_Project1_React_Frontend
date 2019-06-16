638be7bdac07e49fe867eaa456b7e66c6fb105d5
Chris Underwood <cunderwoodmn@gmail.com>
2019-06-15 19:09:59 -0700

    add style dynamically depending on how many users are showing

--- src/App.js
+++ src/App.js
@@ -84,10 +84,20 @@ class App extends Component {
       );
       styleButton.backgroundColor = 'red';
     }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}> This is really working!</p>
         <button style={styleButton} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
